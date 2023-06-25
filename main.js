f17aae20a1bc269b07e0d7e455d8b96745812005
Danny Hsu <danny539425@gmail.com>
2023-06-25 22:53:47 +0800

    0625

--- main.js
+++ main.js
@@ -21,38 +21,43 @@ function rollAll() {
   const reelList = document.querySelectorAll('.slot > .reel');
   let Mycase = Math.floor(Math.random() * 10); // 0 - 9
   console.log(Mycase);
-  switch (Mycase){
-    case 0:
-      console.log("佔領券");
-      break;
-    case 1:
-      console.log("無效券");
-      break;
-    case 2:
-      console.log("搶奪券");
-      break;
-    case 3:
-      console.log("300元");
-      break;
-    case 4:
-    case 5:
-      console.log("100元");
-      break;
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-      console.log("50元");
-      break;
-    default:
-      break;  
-    }
+  
   // console.log(Math.floor(Math.random() * 10)); // 0 - 9
 
   Promise
   .all(  [...reelList].map((reel) => roll(reel)))
   .then((delta) => {
-    console.log(delta)
+    
+    switch (Mycase){
+      case 0:
+        console.log("佔領券");
+        document.querySelector(".say").innerHTML = "佔領券";
+        // const winCls = "win1";
+        // document.querySelector(".slot").classList.add(winCls);
+        // setTimeout(() => document.querySelector(".slots").classList.remove(winCls), 2000)
+        break;
+      case 1:
+        document.querySelector(".say").innerHTML = "無效券";
+        break;
+      case 2:
+        document.querySelector(".say").innerHTML = "搶奪券";
+        break;
+      case 3:
+        document.querySelector(".say").innerHTML = "300元";
+        break;
+      case 4:
+      case 5:
+        document.querySelector(".say").innerHTML = "100元";
+        break;
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+        document.querySelector(".say").innerHTML = "50元";
+        break;
+      default:
+        break;  
+    }
   })
 }
 
